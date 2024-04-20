f6553e4cdbc57e6b5cabc9941ab7f6161599f0ed
Aviel Bitton <aviel.bitton@nielsen.com>
2024-04-20 18:26:14 +0300

    end position

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,27 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import UserTable from "./components/user-table/UserTable";
+import { useState } from "react";
 import Counter from "./components/counter/Counter";
 import ToDoList from "./components/to-do-list/ToDoList";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [showData, setShowData] = useState(false);
-
-  useEffect(() => {
-    const getUsersData = async () => {
-      const fetchedUsers = await axios.get("http://127.0.0.1:8090/api/users");
-      setUsers(fetchedUsers.data.users);
-    };
-
-    getUsersData();
-  }, []);
-
-  const handleButton = () => {
-    setShowData(!showData);
-  };
-
+  const [showToDoList, setShowToDoList] = useState(false);
   return (
     <div>
-      <ToDoList label={"To Do"} />
-      <ToDoList label={"Not To Do"} />
+      <button
+        onClick={() => {
+          setShowToDoList(!showToDoList);
+        }}
+      >
+        {showToDoList ? "not show" : " show"} to do list
+      </button>
+      {showToDoList && (
+        <>
+          <ToDoList label={"To Do"} />
+
+          <ToDoList label={"Not To Do"} />
+        </>
+      )}
+
       <Counter />
-      <button onClick={handleButton}>Show Data</button>
-      {showData && <UserTable users={users} />}
     </div>
   );
 }
